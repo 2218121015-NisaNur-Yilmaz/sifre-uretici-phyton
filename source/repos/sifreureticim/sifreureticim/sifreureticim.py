15191c259655df0e8644c2dd02cbe79c05e39abd
2218121015-NisaNur-Yilmaz <126173831+2218121015-NisaNur-Yilmaz@users.noreply.github.com>
2024-12-24 17:34:25 +0300

    sifre uzunlugu 13

--- source/repos/sifreureticim/sifreureticim/sifreureticim.py
+++ source/repos/sifreureticim/sifreureticim/sifreureticim.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generate_password(length=12 , use_uppercase=False, use_numbers=False, use_symbols=False):
+def generate_password(length=13, use_uppercase=False, use_numbers=False, use_symbols=False):
     """
     Rastgele �ifre �reten bir fonksiyon.
 
